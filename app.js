4fb9bd5763dd37e5c0077cc9d926efbd5ce9620f
dawaedina27 <dawaedina27@gmail.com>
2026-04-05 19:43:15 +0300

    Add root route and server diagnostics

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// explicit root route in case static serving is bypassed
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/api/greet', (req, res) => {
   const name = req.query.name || 'Guest';
   res.json({ message: `Hello, ${name}!` });
--- main.js
+++ main.js
@@ -2,6 +2,28 @@ const app = require('./app');
 
 const PORT = 4000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+server.on('close', () => {
+  console.error('Server closed unexpectedly.');
+});
+
+process.on('exit', (code) => {
+  console.log(`Process exiting with code ${code}`);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err);
+  process.exit(1);
 });
